02dc28d480e65ae053c69a6c7125046655dea58e
Adil Ahamed <adilamed07@gmail.com>
2024-02-25 14:18:47 +0300

    second commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Adil Ahamed</h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Web Developer</p>
+         <p className="text-base md:text-xl mb-3 font-medium">Software Developer</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
             As a Software Developer, I possess arsenal of skills in HTML, CSS, Javascript, React, and Tailwind.
             Explore my portfolio to witness a journey of turning challenges into opportunities through code.
